9040f2254e09a57b826dbf6a2728b95d4c7c767c
houa cha <itshoua@gmail.com>
2020-01-30 08:32:59 -0600

    initial

---
+++ CapStoneApp/CapStoneApp/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CapStoneApp.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Party { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ CapStoneApp/CapStoneApp/Models/Forum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CapStoneApp.Models
+{
+    public class Forum
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [ForeignKey("Client")]
+        public int? ClientId { get; set; }
+        public virtual Client Client { get; set; }
+    }
+}
